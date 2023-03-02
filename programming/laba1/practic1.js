10c6b14204873e4773b11115ad9e383c1bed159d
Romankov Artyom <romankov.epoxy@gmail.com>
2023-03-02 22:18:27 +0700

    modified:   programming/laba1/practic1.js

--- programming/laba1/practic1.js
+++ programming/laba1/practic1.js
@@ -1,12 +1,25 @@
-function min(arr){
-    let min = arr[0];
-    for (let i = 1; i < arr.length; i++){
+function min(arr, start) {
+    let min = arr[start];
+    let minI = start;
+    for (let i = start+1; i < arr.length; i++){
         if (min > arr[i]) {
-            min = arr[i]; 
+            min = arr[i];
+            minI = i;
         }
     }
-    return min;
+    return minI;
 }
 
-let Arr = [3,-1,8,1]
-console.log(min(Arr))
+function sort(arr) {
+    let m, cup;
+    for (let i = 0; i < arr.length; i++) {
+        m = min(arr, i);
+        cup = arr[i];
+        arr[i] = arr[m];
+        arr[m] = cup;
+    }
+    return arr;
+}
+
+let Arr = [-1,3,8,1];
+console.log(sort(Arr));
